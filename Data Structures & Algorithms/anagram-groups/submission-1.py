4d875ec2dbcf02d12e4c821e547ed7cc97513d14
ArkaPrabhaChowdhury <92904175+ArkaPrabhaChowdhury@users.noreply.github.com>
2026-08-24 21:39:24 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        shell = {}
+        sortedStrings = {}
+        for i in range(len(strs)):
+            sortedWord = (" ").join(sorted(strs[i]))
+            sortedStrings[i] = sortedWord
+            shell[sortedWord] = []
+        
+        for i in range(len(sortedStrings)):
+            sortedWord = sortedStrings[i]
+            ogWord = strs[i]
+            shell[sortedWord].append(ogWord)
+        return list(shell.values())
